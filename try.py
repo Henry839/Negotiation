4be2e628ae8f7e103fb548ab423c5cb8b4691de3
Henry839 <2868572697@qq.com>
2024-01-03 21:56:24 +0800

    unfinished save

--- try.py
+++ try.py
@@ -5,9 +5,7 @@ env = gym.make("colored_trail/ColoredTrailEnv-v0", render_mode="human", size=5)
 observation, info = env.reset(seed=42)
 for _ in range(1000):
     action = env.action_space.sample()  # this is where you would insert your policy
-    print(action)
     observation, reward, terminated, truncated, info = env.step(action)
-    print(observation)
 
     if terminated or truncated:
         observation, info = env.reset()
